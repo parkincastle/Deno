50eca7b6ce3046500ba00230abfd7f611179cfbd
parkincastle <shfoemesmsrPwjd1@gmail.com>
2021-04-13 11:14:33 +0900

    deno

--- Import.ts
+++ Import.ts
@@ -4,5 +4,3 @@ import { add } from './play-deno.ts';   //ex
 
 // Remote Import : 리모트에서 URL로 JavaScript을 바로 가져오는 방식
 import { add } from 'https://x.nest.land/ramda@0.27.0/source/index.js'; //ex
-
-
---
+++ server.ts
@@ -0,0 +1,28 @@
+// 실행시 deno run이 아니라 deno run --allow-net으로 해야한다.
+// 왜냐 permission과 관련한 flag를 주지 않으면 에러가 발생하기 때문
+// 즉 보안이 강화되어서 그렇다.
+import { serve } from "https://deno.land/std@0.83.0/http/server.ts";
+
+const server = serve({ hostname: "0.0.0.0", port: 8080 });
+console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/ 🦕`);
+
+(async function handleHttpRequest () {
+  for await (const request of server) {
+    let bodyContent = "Your user-agent is:\n\n";
+    bodyContent += request.headers.get("user-agent") || "Unknown";
+
+    request.respond({
+      status: 200,
+      body: `
+      Hello Deno!
+                    __
+                    / _)
+          _.----._/ /
+          /         /
+      __/ (  | (  |
+      /__.-'|_|--|_|
+      `
+    });
+  }
+})()
+
